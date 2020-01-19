45cfd87e30baa340ac729d00333a69697f83641c
Threetwosevensixseven <robin.guest@gmail.com>
2020-01-19 16:23:16 -0500

    V1.55 Beta release.

--- src/asm/main.asm
+++ src/asm/main.asm
@@ -1,10 +1,10 @@
-; main.asm
+ ; main.asm
                                                         ; Assembles with regular version of Zeus (not Next version),
 zeusemulate             "48K", "RAW", "NOROM"           ; because that makes it easier to assemble dot commands
 zoSupportStringEscapes  = true;                         ; Download Zeus.exe from http://www.desdes.com/products/oldfiles/
 optionsize 5
 CSpect optionbool 15, -15, "CSpect", false              ; Option in Zeus GUI to launch CSpect
-RealESP optionbool 80, -15, "Real ESP", true            ; Launch CSpect with physical ESP in USB adaptor
+RealESP optionbool 80, -15, "Real ESP", false           ; Launch CSpect with physical ESP in USB adaptor
 UploadNext optionbool 160, -15, "Next", false           ; Copy dot command to Next FlashAir card
 ErrDebug optionbool 212, -15, "Debug", false            ; Print errors onscreen and halt instead of returning to BASIC
 
